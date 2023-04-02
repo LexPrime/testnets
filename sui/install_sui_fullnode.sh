aab7c98c3892f9b9592f6b1281916beaa8cad67e
lexprime <lexprime.official@gmail.com>
2023-04-03 01:49:13 +0300

    Implement gum

--- sui/install_sui_fullnode.sh
+++ sui/install_sui_fullnode.sh
@@ -2,62 +2,280 @@
 
 DOCKER_COMPOSE_VERSION="v2.17.0"
 
+# Install gum
+if [[ ! -x "$(command -v gum)" ]]; then
+  sudo mkdir -p /etc/apt/keyrings
+  curl -fsSL https://repo.charm.sh/apt/gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/charm.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/charm.gpg] https://repo.charm.sh/apt/ * *" | sudo tee /etc/apt/sources.list.d/charm.list
+  sudo apt update && sudo apt install -y gum > /dev/null
+fi
+
+# Title
+gum style --foreground 4 --border-foreground 4 --border double --bold --align center --width 50 --margin "2 20" --padding "1 1" 'SUI NODE INSTALLER' 'by Darksiders Staking'
+
+# Choose option
+USER_PICK=$(gum choose --cursor.foreground=4 "Install fullnode" "Install fullnode + monitoring")
+
+
 # Install docker
-sudo apt-get update > /dev/null
 if [[ ! -x "$(command -v docker)" ]]; then
-  echo -e "Docker not installed. Installing..."
+  sudo apt-get update > /dev/null
+  gum style --foreground 4 --align left --margin "1 1" "Docker not installed. Installing..."
   sudo apt-get remove docker docker-engine docker.io containerd runc > /dev/null
-  sudo apt-get install -y curl
+  sudo apt-get install -y curl > /dev/null
   curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
   echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   sudo apt-get update > /dev/null
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io > /dev/null
-  echo -e "Done! $(docker --version)"
+  gum style --foreground 4 --align left --margin "1 1" "Done! $(docker --version)"
 else
-  echo -e "Docker installed. $(docker --version)"
+  gum style --foreground 4 --align left --margin "1 1" "Docker installed. $(docker --version)"
 fi
 
 
 # Install docker-compose
-if [[ ! -x "$(command -v docker)" ]]; then
-  echo -e "Installing docker-compose..."
+if [[ ! -x "$(command -v docker-compose)" ]]; then
+  gum style --foreground 4 --align left --margin "2 2" "Installing docker-compose..."
   curl -sL https://github.com/docker/compose/releases/download/$DOCKER_COMPOSE_VERSION/docker-compose-linux-x86_64 -o docker-compose
   chmod +x docker-compose
   sudo mv docker-compose /usr/local/bin
-  echo -e "Done! Docker-compose version: $(docker-compose --version | cut -d ' ' -f 4)"
+  gum style --foreground 4 --align left --margin "1 1" "Done! Docker-compose version: $(docker-compose --version | cut -d ' ' -f 4)"
 else
-  echo -e "Docker-compose installed. Docker-compose version: $(docker-compose --version | cut -d ' ' -f 4)"
+  gum style --foreground 4 --align left --margin "1 1" "Docker-compose installed. Docker-compose version: $(docker-compose --version | cut -d ' ' -f 4)"
 fi
 
 
 # Create config files
 if [[ -d $HOME/.sui ]]; then
-  echo -e "Sui folder exist"
+  cd $HOME/.sui
+  docker-compose down
 else
   mkdir $HOME/.sui
 fi
+cd $HOME/.sui
 
-echo '
-version: "3.9"
+
+# Create docker-compose file
+echo 'version: "3.9"
+
+volumes:
+  suidb:
+  prometheus_data:
+  grafana_data:
+
+networks:
+  sui-net:
+    driver: bridge
+
 services:
   fullnode:
     image: lexprime/sui:latest
     container_name: sui-node
+    restart: on-failure
     ports:
       - "8084:8084/udp"
       - "9000:9000"
       - "9184:9184"
+    expose:
+      - 9000
+    networks:
+      - sui-net
     volumes:
       - ./fullnode-template.yaml:/sui/fullnode.yaml:ro
       - ./genesis.blob:/sui/genesis.blob:ro
       - suidb:/sui/db:rw
     command: ["/usr/local/bin/sui-node", "--config-path", "fullnode.yaml"]
+
+  prometheus:
+    image: prom/prometheus
+    container_name: prometheus
+    restart: on-failure
+    expose:
+      - 9090
+    networks:
+      - sui-net
+    volumes:
+      - ./prometheus:/etc/prometheus
+      - prometheus_data:/prometheus
+    command:
+      - "--config.file=/etc/prometheus/prometheus.yml"
+      - "--storage.tsdb.path=/prometheus"
+      - "--web.console.libraries=/etc/prometheus/console_libraries"
+      - "--web.console.templates=/etc/prometheus/consoles"
+      - "--storage.tsdb.retention.time=200h"
+      - "--web.enable-lifecycle"
+
+  node-exporter:
+    image: prom/node-exporter
+    container_name: node-exporter
+    restart: on-failure
+    volumes:
+      - /proc:/host/proc:ro
+      - /sys:/host/sys:ro
+      - /:/rootfs:ro
+    command:
+      - "--path.procfs=/host/proc"
+      - "--path.rootfs=/rootfs"
+      - "--path.sysfs=/host/sys"
+      - "--collector.filesystem.ignored-mount-points=^/(sys|proc|dev|host|etc)($$|/)"
+    expose:
+      - 9100
+    networks:
+      - sui-net
+
+  grafana:
+    image: grafana/grafana
+    container_name: grafana
+    volumes:
+      - grafana_data:/var/lib/grafana
+      - ./grafana/provisioning:/etc/grafana/provisioning
+    environment:
+      - GF_SECURITY_ADMIN_USER=${GRAFANA_USER:-admin}
+      - GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASSWORD:-admin}
+      - GF_USERS_ALLOW_SIGN_UP=false
+      - GF_SECURITY_DISABLE_BRUTE_FORCE_LOGIN_PROTECTION=true
+    restart: on-failure
+    expose:
+      - 3000
+    ports:
+      - "3555:3000"
+    networks:
+      - sui-net
+
+  cadvisor:
+    image: gcr.io/cadvisor/cadvisor
+    container_name: cadvisor
+    volumes:
+      - /:/rootfs:ro
+      - /var/run:/var/run:rw
+      - /sys:/sys:ro
+      - /var/lib/docker:/var/lib/docker:ro
+      - /cgroup:/cgroup:ro
+    restart: on-failure
+    expose:
+      - 8080
+    networks:
+      - sui-net' > $HOME/.sui/docker-compose.yaml
+
+
+# Create docker-compose.monitoring file
+echo '
 volumes:
-  suidb:' > $HOME/.sui/docker-compose.yaml
+  prometheus_data:
+  grafana_data:
+
+services:
+  prometheus:
+    image: prom/prometheus
+    container_name: prometheus
+    restart: on-failure
+    expose:
+      - 9090
+    networks:
+      - sui-net
+    volumes:
+      - ./prometheus:/etc/prometheus
+      - prometheus_data:/prometheus
+    command:
+      - "--config.file=/etc/prometheus/prometheus.yml"
+      - "--storage.tsdb.path=/prometheus"
+      - "--web.console.libraries=/etc/prometheus/console_libraries"
+      - "--web.console.templates=/etc/prometheus/consoles"
+      - "--storage.tsdb.retention.time=200h"
+      - "--web.enable-lifecycle"
+
+  node-exporter:
+    image: prom/node-exporter
+    container_name: node-exporter
+    restart: on-failure
+    volumes:
+      - /proc:/host/proc:ro
+      - /sys:/host/sys:ro
+      - /:/rootfs:ro
+    command:
+      - "--path.procfs=/host/proc"
+      - "--path.rootfs=/rootfs"
+      - "--path.sysfs=/host/sys"
+      - "--collector.filesystem.ignored-mount-points=^/(sys|proc|dev|host|etc)($$|/)"
+    expose:
+      - 9100
+    networks:
+      - sui-net
+
+  grafana:
+    image: grafana/grafana
+    container_name: grafana
+    volumes:
+      - grafana_data:/var/lib/grafana
+    environment:
+      - GF_SECURITY_ADMIN_USER=${GRAFANA_USER:-admin}
+      - GF_SECURITY_ADMIN_PASSWORD=${GRAFANA_PASSWORD:-admin}
+      - GF_USERS_ALLOW_SIGN_UP=false
+      - GF_SECURITY_DISABLE_BRUTE_FORCE_LOGIN_PROTECTION=true
+    restart: on-failure
+    expose:
+      - 3000
+    ports:
+      - "3555:3000"
+    networks:
+      - sui-net
+
+  cadvisor:
+    image: gcr.io/cadvisor/cadvisor
+    container_name: cadvisor
+    volumes:
+      - /:/rootfs:ro
+      - /var/run:/var/run:rw
+      - /sys:/sys:ro
+      - /var/lib/docker:/var/lib/docker:ro
+      - /cgroup:/cgroup:ro
+    restart: on-failure
+    expose:
+      - 8080
+    networks:
+      - sui-net' > $HOME/.sui/docker-compose.monitoring.yaml
 
 
+# Create docker-compose.indexer file
+echo 'version: "3.9"
+volumes:
+  postgres_db:
+
+networks:
+  sui-net:
+    driver: bridge
+
+  postgres:
+    image: postgres:15
+    container_name: postgres
+    restart: on-failure
+    environment:
+      POSTGRES_USER: postgres
+      POSTGRES_PASSWORD: postgres
+      POSTGRES_DB: sui_indexer_db
+    expose:
+      - 5432
+    networks:
+      - sui-net
+    volumes:
+      - postgres_db:/var/lib/postgresql/data:rw
+    command: ["postgres", "-cshared_preload_libraries=pg_stat_statements"]
+
+  indexer:
+    image: lexprime/sui:latest
+    container_name: sui-indexer
+    restart: on-failure
+    networks:
+      - sui-net
+    volumes:
+      - ./start_indexer.sh:/sui/start_indexer.sh:ro
+    tty: true
+    command: ["/bin/bash"]' > $HOME/.sui/docker-compose.indexer.yaml
+
+
+# Create fullnode config file
 tee $HOME/.sui/fullnode-template.yaml > /dev/null <<EOF
 # Update this value to the location you want Sui to store its database
 db-path: "/sui/db"
@@ -93,12 +311,71 @@ EOF
 
 
 # Get genesis
-echo -e "Downloading genesis..."
+gum style --foreground 4 --align left --margin "1 1" "Downloading genesis..."
 curl -Ls https://github.com/MystenLabs/sui-genesis/raw/main/testnet/genesis.blob > $HOME/.sui/genesis.blob
-echo -e "Done! Genesis shasum $(sha256sum $HOME/.sui/genesis.blob)"
+gum style --foreground 4 --align left --margin "1 1" "Done! Genesis shasum $(sha256sum $HOME/.sui/genesis.blob | cut -d ' ' -f 1)"
 
 
-# Start sui-node
-cd $HOME/.sui
-docker-compose up -d
-echo -e "Setup complete! Check logs with docker logs -f sui-node"
+# Build docker compose and start
+gum style --foreground 4 --align left --margin "1 1" "Creating services..."
+if [[ $USER_PICK == "Install fullnode" ]]; then
+  docker-compose up -d fullnode
+else
+  if [[ ! -d $HOME/.sui/prometheus ]]; then
+    mkdir $HOME/.sui/prometheus
+    mkdir -p $HOME/.sui/grafana/provisioning/dashboards
+    curl -Ls https://grafana.com/api/dashboards/18297/revisions/1/download > $HOME/.sui/grafana/provisioning/sui_node.json
+    echo 'ApiVersion: 1
+datasources:
+  - access: proxy
+    editable: true
+    name: Prometheus
+    orgId: 1
+    type: prometheus
+    url: http://prometheus:9090/
+    version: 1' > $HOME/.sui/grafana/provisioning/datasources/prometheus.yml
+  fi
+  echo "global:
+    scrape_interval:     15s
+    evaluation_interval: 15s
+
+# Load and evaluate rules in this file every 'evaluation_interval' seconds.
+rule_files:
+  - "alert.rules"
+
+# A scrape configuration containing exactly one endpoint to scrape.
+scrape_configs:
+  - job_name: 'node-exporter'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['node-exporter:9100']
+
+  - job_name: 'cadvisor'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['cadvisor:8080']
+
+  - job_name: 'prometheus'
+    scrape_interval: 10s
+    static_configs:
+      - targets: ['localhost:9090']
+
+  - job_name: 'sui-node'
+    scrape_interval: 10s
+    metrics_path: /metrics
+    static_configs:
+      - targets: ['fullnode:9184']
+
+  - job_name: 'grafana'
+    scrape_interval: 5s
+    static_configs:
+      - targets: ['grafana:3000']" > $HOME/.sui/prometheus/prometheus.yml
+  docker-compose up -d
+fi
+
+# Complete
+gum style --foreground 4 --align left --margin "1 1" "Setup complete!"
+gum style --foreground 4 --align left --margin "1 1" "Check logs with docker logs -f sui-node"
+
+# Credits
+gum style --foreground 4 --border-foreground 4 --border double --bold --align center --width 50 --margin "2 20" --padding "1 1" 'Created by Lex_Prime from Darksiders Staking' 'Please follow me on' 'Github: https://github.com/LexPrime' 'Twitter: https://twitter.com/Lex__Prime' 'Medium: https://medium.com/@lexprime'
